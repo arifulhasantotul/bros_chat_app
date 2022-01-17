ea33b9bddd7d091131dd5d9dd26e728eff886b2a
arifulhasantotul <mdahtotul@gmail.com>
2022-01-18 00:55:01 +0600

    model bug

--- routers/inboxRouter.js
+++ routers/inboxRouter.js
@@ -1,11 +1,34 @@
 // external imports
 const express = require("express");
+const {
+  getInbox,
+  searchUser,
+  addConversation,
+  sendMessage,
+  getMessages,
+} = require("../controllers/inboxController");
+const { checkLogin } = require("../middlewares/common/checkLogin");
+const decorateHTMLResponse = require("../middlewares/common/decorateHTMLResponse");
+const attachmentUpload = require("../middlewares/Inbox/attachmentUpload");
 
 // internal imports
 
 const router = express.Router();
 
 // inbox page
+router.get("/", decorateHTMLResponse("Inbox"), getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
 
 // module export
 module.exports = router;
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,11 +1,28 @@
 // external imports
 const express = require("express");
+const { getUsers, addUser } = require("../controllers/usersController");
+const decorateHTMLResponse = require("../middlewares/common/decorateHTMLResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidator");
 
 // internal imports
 
 const router = express.Router();
 
 // users page
+router.get("/", decorateHTMLResponse("Users"), getUsers);
+
+// add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 // module export
 module.exports = router;
